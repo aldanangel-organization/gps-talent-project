676223eaeae5799b6087f22d3262df4402eca486
AngelAldana <aldanangel05@gmail.com>
2024-12-17 00:56:06 -0500

    fix comma error

--- application/.jenkins/flows/build.groovy
+++ application/.jenkins/flows/build.groovy
@@ -6,7 +6,7 @@ def pipeline = new build_ci_shared_library()
 
 pipeline.execute(
     message: 'PIPELINE',
-    appName: 'gps-talent-proyect'
+    appName: 'gps-talent-proyect',
     repository: 'aws_dev',
     type: 'lambda'
 )
