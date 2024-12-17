ddd93ef9387bb055ea992b5932b4c5f0b650ee16
AngelAldana <aldanangel05@gmail.com>
2024-12-17 00:14:26 -0500

    test webhook

--- application/.jenkins/build.groovy
+++ application/.jenkins/build.groovy
@@ -5,5 +5,5 @@ import ci.devops.aldanangel.pipelines.jenkins.pipelines.build_ci_shared_library
 def pipeline = new build_ci_shared_library()
 
 pipeline.execute(
-    message: 'TEST'
+    message: 'PIPELINE'
 )
