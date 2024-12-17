94706026d343b5085f188959fe2cb2c9a9586984
AngelAldana <aldanangel05@gmail.com>
2024-12-17 00:15:59 -0500

    fix import value

--- application/.jenkins/build.groovy
+++ application/.jenkins/build.groovy
@@ -1,6 +1,6 @@
 @Library('ci_shared_library')
 
-import ci.devops.aldanangel.pipelines.jenkins.pipelines.build_ci_shared_library
+import ci.devops.aldanangel.pipelines.build_ci_shared_library
 
 def pipeline = new build_ci_shared_library()
 
