aa09f76c3c5833b7ad2f1be3b15f4e29366713ac
AngelAldana <aldanangel05@gmail.com>
2024-12-17 00:55:26 -0500

    add properties

--- application/.jenkins/flows/build.groovy
+++ application/.jenkins/flows/build.groovy
@@ -5,5 +5,8 @@ import ci.devops.aldanangel.pipelines.build_ci_shared_library
 def pipeline = new build_ci_shared_library()
 
 pipeline.execute(
-    message: 'PIPELINE'
+    message: 'PIPELINE',
+    appName: 'gps-talent-proyect'
+    repository: 'aws_dev',
+    type: 'lambda'
 )
