9ba6a66bf2acbc2fe1c1fd625c69b83d4d568a24
AngelAldana <aldanangel05@gmail.com>
2024-12-18 13:46:49 -0500

    update message

--- hello_world/app.py
+++ hello_world/app.py
@@ -36,7 +36,7 @@ def lambda_handler(event, context):
     return {
         "statusCode": 200,
         "body": json.dumps({
-            "message": "hello gps talent",
+            "message": "hello world",
             # "location": ip.text.replace("\n", "")
         }),
     }
